eadbd8599ac1649e6fb8bb17b9e704e1ba400c7d
muffin <lement38641@gmail.com>
2023-11-08 16:58:32 +0900

    231108 campus

--- Study_04/Study_04/Study_04_01.cpp
+++ Study_04/Study_04/Study_04_01.cpp
@@ -56,14 +56,19 @@ int main()
 
 	// 포인터 변수
 	// 주소를 저장하는 변수
+	// 자료형 + * 변수명
+	// 해당 포인터에게 전달된 주소를 해석하는 단위
 	int i = 100;
-	int* pInt = &i;
+	float f = 3.f;
+
+	int* pInt = (int*)&f;
 	
 	// 주소로 접근
-	(*pInt) = 100;
+	i = *pInt;
 
 	// 주소의 단위
 	// BYTE
+
 
 
 	return 0;
